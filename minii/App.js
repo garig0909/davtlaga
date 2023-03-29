2cf2be65362c998d2a90dfdd64a7f46bdb981a15
garig09 <odkomonhbayr@gmail.com>
2023-03-29 18:58:00 +0800

    dolder

--- minii/App.js
+++ minii/App.js
@@ -1,14 +1,40 @@
 /** @format */
 
-import { View, Text, SafeAreaView } from "react-native";
 import React from "react";
+import {
+  View,
+  Text,
+  SafeAreaView,
+  StyleSheet,
+  Platform,
+  StatusBar,
+} from "react-native";
 
 export default function App() {
   return (
-    <SafeAreaView>
-      <View>
-        <Text>odriin mend</Text>
+    <SafeAreaView styles={styles.container}>
+      <View styles={styles.view1}>
+        <Text style={styles.te}>odriin mend</Text>
       </View>
     </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "red",
+    margin: 20,
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+  },
+  view1: {
+    height: 50,
+    borderWidth: 1,
+    justifyContent: "center",
+  },
+  tex: {
+    fontSize: 25,
+    fontWeight: "bold",
+    marginLeft: 50,
+  },
+});
